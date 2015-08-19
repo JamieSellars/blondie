77eba57a82f2e25d23cef58cb58cdae5ac2fba52
Jamie Sellars <jamie.sellars@perisher.com.au>
2015-08-19 11:01:59 +1000

    ADD - AngularJS Authentication Service

--- app/controllers/controller.settings.js
+++ app/controllers/controller.settings.js
@@ -4,7 +4,7 @@
 *   @description: settomgs functions shared on the view - user settings / category
 *   @author:      jamie sellars (@goingsideways on github)
 **/
-angular.module('app').controller('settingsController', ['$scope','settingsUserService', function($scope,settingsUserService){
+angular.module('app').controller('settingsController', ['$scope','settingsUserService','alertService', function($scope,settingsUserService,alertService){
 
     vm = this;
     vm.title = "settings";
@@ -19,15 +19,19 @@ angular.module('app').controller('settingsController', ['$scope','settingsUserSe
 
     vm.users = {
       /** FORM DATA **/
-      save: function(){
-        // Save form data to database
-        // Creates a user and will return the username in plain text.
-        // user will need to change their password on first login
 
+      // Save form data to database
+      // Creates a user and will return the username in plain text.
+      // user will need to change their password on first login
+      save: function(){
         // create username string
         vm.users.formdata.username = vm.users.formdata.firstname.toLowerCase().charAt(0).concat(vm.users.formdata.lastname.substr(0, vm.users.formdata.lastname.length));
-
-        settingsUserService.save(vm.users.formdata);
+        settingsUserService.save(vm.users.formdata).then(function(d){
+            console.log("Saved");
+        }, function(err){
+            console.log(err);
+            alertService('danger', 'oops!', 'Could not create user: [' + err.status + '] ' + err.data)
+        });
 
       }
     };
